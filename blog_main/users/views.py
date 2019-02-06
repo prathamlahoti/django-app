6a219a5bc71bad8c8aa91dcd13c589f2e576274a
ajmoriss <ajmoriss444@gmail.com>
2019-02-06 13:14:02 -0800

    Signup and Login components established.

--- blog_main/users/views.py
+++ blog_main/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import UserSignupForm
 
-# Create your views here.
+
+def signup(request):
+	"""User Registration View"""
+	if request.method == 'POST':
+		signupForm = UserSignupForm(request.POST)
+		if signupForm.is_valid():
+			signupForm.save()
+			messages.success(request, 'You were successfully signed up!')
+			return redirect('login')
+	else:
+		signupForm = UserSignupForm()
+	return render(request, "signup.html", {'form': signupForm})
